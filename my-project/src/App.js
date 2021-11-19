2a64a0759de1ae96e8f0c1ee14cf60afbf3fee19
Kostya <Kpstya@mail.ru>
2021-11-20 01:36:13 +0300

    first HW

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const Message = ({name, text}) => {
+  return <div className={'block'}>
+    <div className={'block__name'}>сообщение от {name}</div>
+    <div className={'block__text'}>{text}</div>
+  </div>
+}
 
 function App() {
+
+  const [messages] = useState(
+      [
+        {name: 'Сергей Петров', text: 'Привет, как дела?'},
+        {name: 'Мирон Фёдоров', text: 'Кто убил Марка?'},
+        {name: 'Пётр Александров', text: 'Что я здесь делаю?'},
+      ]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        {messages.map(({name, text}, index) =>
+            <Message key={index} name={name} text={text}/>
+        )}
+
+      </div>
   );
 }
 
