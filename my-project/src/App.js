7098dc58cdb3afbcae348d27b2ac63d612626a74
Kostya <Kpstya@mail.ru>
2021-11-22 14:14:45 +0300

    дз 2

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
 import {useState} from "react";
-
-const Message = ({name, text}) => {
-  return <div className={'block'}>
-    <div className={'block__name'}>сообщение от {name}</div>
-    <div className={'block__text'}>{text}</div>
-  </div>
-}
+import {Message} from "./components/Message";
+import {Header} from "./components/Header";
+import MessageList from "./components/MessageList"
 
 function App() {
 
-  const [messages] = useState(
-      [
-        {name: 'Сергей Петров', text: 'Привет, как дела?'},
-        {name: 'Мирон Фёдоров', text: 'Кто убил Марка?'},
-        {name: 'Пётр Александров', text: 'Что я здесь делаю?'},
-      ]
-  );
+    const [messages] = useState(
+        [
+            {name: 'Сергей Петров', text: 'Привет, как дела?'},
+            {name: 'Мирон Фёдоров', text: 'Кто убил Марка?'},
+            {name: 'Пётр Александров', text: 'Что я здесь делаю?'},
+        ]
+    );
 
-  return (
-      <div className="App">
+    const data = {
+        date: new Date(),
+        name: 'Konstantin'
+    }
 
-        {messages.map(({name, text}, index) =>
-            <Message key={index} name={name} text={text}/>
-        )}
-
-      </div>
-  );
+    return (
+        <div className="App">
+            <Header date={data.date} name={data.name}/>
+            <h1>Домашнее задание №1</h1>
+            {messages.map(({name, text}, index) =>
+                <Message key={index} name={name} text={text}/>
+            )}
+            <h1>Домашнее задание №2</h1>
+            <MessageList/>
+        </div>
+    );
 }
 
 export default App;
